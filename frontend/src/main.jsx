9c5ffa9c80f046c713fbeedee82002a2024c476f
MOULI3636 <banerjeemouli26@gmail.com>
2026-03-18 16:58:13 +0530

    Fix deployed routing with HashRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
@@ -8,11 +8,11 @@ import { AuthProvider } from './context/AuthContext';
 import { OrderProvider } from './context/OrderContext';
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+    <HashRouter>
         <AuthProvider>
             <OrderProvider>
                 <App />
             </OrderProvider>
         </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
 );
